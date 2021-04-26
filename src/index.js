897a02e2098bf6f1fe082ce8aaf84c29396c38f9
Nathan <nathanbonnet@wifibridge-3.home>
2021-04-26 20:41:07 +0200

    ajout du dropdown

--- src/index.js
+++ src/index.js
@@ -2,83 +2,117 @@ import data from '../data.json';
 
 console.log(data);
 
-const toto = data.media.map(media => media.tags.map(tags => tags, typeof tags));
-
-function onlyUnique(value, index, self) {
-    return self.indexOf(value) === index;
-  }
-  
-  // usage example:
-  var unique = toto.filter(onlyUnique);
-  
-  console.log(unique); // ['a', 1, 2, '1']
+let tags = [... new Set(data.photographers.map(photographer => photographer.tags).reduce((acc, value) => acc.concat(value, [])))];
 
 const bloc_main = document.getElementById("bloc_main");
+const nav = document.getElementById("nav");
+let selectedTags = [];
+tags.forEach(unique => {
+    let lien_tag_nav = document.createElement("a");
+    let tag_nav = document.createElement("p");
 
-for (var i in data.photographers) {
-    //création de tous les élements nécessaire pour chaque photographe
-    const bloc = document.createElement("div");
-    const bloc_photographers = document.createElement("a");
-    const image = document.createElement("img");
-    const photographers = document.createElement("h2");
-    const photographers_city = document.createElement("h3");
-    const photographers_tagLine = document.createElement("p");
-    const photographers_price = document.createElement("p");
-    const bloc_photographers_tags = document.createElement("div");
+    nav.append(lien_tag_nav);
+    lien_tag_nav.append(tag_nav);
 
-    //affichage de tous les élements nécessaire pour chaque photographe
-    photographers.textContent = data.photographers[i].name;
-    photographers_city.textContent = data.photographers[i].city + ", " + data.photographers[i].country;
-    photographers_tagLine.textContent = data.photographers[i].tagline;
-    photographers_price.textContent = data.photographers[i].price + "€/jour";
-    image.src =  "../img/Sample_Photos-2/Photographers_ID_Photos/"+ data.photographers[i].portrait;
+    lien_tag_nav.setAttribute("class", "lien_tag_photographe");
+    tag_nav.setAttribute("class", "photographers_tags");
+    lien_tag_nav.setAttribute("href", "#");
+    tag_nav.setAttribute("aria-label", unique);
+    
+    tag_nav.innerHTML = "#"+ unique;
+    lien_tag_nav.addEventListener("click", () => {
+        let tag = lien_tag_nav.text.replace("#", "");
+        if (selectedTags.includes(tag)) {
+            selectedTags = selectedTags.filter(word => word != tag);
+            tag_nav.setAttribute("class", "photographers_tags");
+        }else {
+            selectedTags.push(tag);
+            tag_nav.setAttribute("class", "lien_tag_photographe_selected photographers_tags");
+        }
+        if(selectedTags.length) {
+            createPhotographers(getPhotographersFromTags(data.photographers, selectedTags));
+        }else {
+            createPhotographers(data.photographers)  
+        }
+    })
+})
 
-    //ajout de tous les élements dans la div parent (bloc) qui elle même est ajouté à (bloc_main)
-    bloc_main.append(bloc);
-    bloc.append(bloc_photographers);
-    bloc_photographers.append(image);
-    bloc_photographers.append(photographers);
-    bloc.append(photographers_city);
-    bloc.append(photographers_tagLine);
-    bloc.append(photographers_price);
-    bloc.append(bloc_photographers_tags);
+createPhotographers(data.photographers)
 
-    //ajout de class à tous les éléments
-    bloc.setAttribute("class", "bloc_photographer");
-    bloc_photographers.setAttribute("href", "photographe.html?id=" + data.photographers[i].id);
-    bloc_photographers.setAttribute("class", "lien_photographe");
-    image.setAttribute("class", "img");
-    photographers.setAttribute("class", "photographers");
-    photographers_city.setAttribute("class", "photographers_city");
-    photographers_tagLine.setAttribute("class", "photographers_tagLine");
-    photographers_price.setAttribute("class", "photographers_price");
-    bloc_photographers_tags.setAttribute("class", "bloc_photographers_tags");
+function createPhotographers(photographers){
+    console.log(photographers)
+    bloc_main.innerHTML = "";
+    photographers.forEach((photographer) => {
+        //création de tous les élements nécessaire pour chaque photographe
+        const bloc = document.createElement("div");
+        const bloc_photographers = document.createElement("a");
+        const image = document.createElement("img");
+        const photographers = document.createElement("h2");
+        const photographers_city = document.createElement("h3");
+        const photographers_tagLine = document.createElement("p");
+        const photographers_price = document.createElement("p");
+        const bloc_photographers_tags = document.createElement("div");
 
-    //ajout de aria-label à tous les éléments
-    bloc_photographers.setAttribute("aria-label", data.photographers[i].name);
-    photographers_city.setAttribute("aria-label", data.photographers[i].city);
-    photographers_tagLine.setAttribute("aria-label", data.photographers[i].tagline);
-    photographers_price.setAttribute("aria-label", data.photographers[i].price + "€/jour");
+        //affichage de tous les élements nécessaire pour chaque photographe
+        photographers.textContent = photographer.name;
+        photographers_city.textContent = photographer.city + ", " + photographer.country;
+        photographers_tagLine.textContent = photographer.tagline;
+        photographers_price.textContent = photographer.price + "€/jour";
+        image.src =  "../img/Sample_Photos-2/Photographers_ID_Photos/"+ photographer.portrait;
 
-    //boucle sur tous les tags de chaque photographe
-    for (var a in data.photographers[i].tags) {
-
-        //création de tous les élements pour les tags de chaque photographe
-        const lien_photographers_tags = document.createElement("a");
-        const photographers_tags = document.createElement("p");
-
-        //affichage de tous les tags nécessaire pour chaque photographe
-        photographers_tags.textContent = "#"+data.photographers[i].tags[a];
-
-        //ajout de tous les tags dans la div parent (bloc_photographers_tags)
-        bloc_photographers_tags.append(lien_photographers_tags);
-        lien_photographers_tags.append(photographers_tags);
+        //ajout de tous les élements dans la div parent (bloc) qui elle même est ajouté à (bloc_main)
+        bloc_main.append(bloc);
+        bloc.append(bloc_photographers);
+        bloc_photographers.append(image);
+        bloc_photographers.append(photographers);
+        bloc.append(photographers_city);
+        bloc.append(photographers_tagLine);
+        bloc.append(photographers_price);
+        bloc.append(bloc_photographers_tags);
 
         //ajout de class à tous les éléments
-        lien_photographers_tags.setAttribute("class", "lien_tag_photographe");
-        photographers_tags.setAttribute("class", "photographers_tags");
-        lien_photographers_tags.setAttribute("href", "#");
-        photographers_tags.setAttribute("aria-label", data.photographers[i].tags[a]);
-    }
+        bloc.setAttribute("class", "bloc_photographer");
+        bloc_photographers.setAttribute("href", "photographe.html?id=" + photographer.id);
+        bloc_photographers.setAttribute("class", "lien_photographe");
+        image.setAttribute("class", "img");
+        photographers.setAttribute("class", "photographers");
+        photographers_city.setAttribute("class", "photographers_city");
+        photographers_tagLine.setAttribute("class", "photographers_tagLine");
+        photographers_price.setAttribute("class", "photographers_price");
+        bloc_photographers_tags.setAttribute("class", "bloc_photographers_tags");
+
+        //ajout de aria-label à tous les éléments
+        image.setAttribute("aria-label", photographer.name);
+        photographers_city.setAttribute("aria-label", photographer.city);
+        photographers_tagLine.setAttribute("aria-label", photographer.tagline);
+        photographers_price.setAttribute("aria-label", photographer.price + "€/jour");
+
+        //boucle sur tous les tags de chaque photographe
+        photographer.tags.forEach((photographer) => {
+            //création de tous les élements pour les tags de chaque photographe
+            const lien_photographers_tags = document.createElement("a");
+            const photographers_tags = document.createElement("p");
+
+            //affichage de tous les tags nécessaire pour chaque photographe
+            photographers_tags.textContent = "#"+photographer;
+
+            //ajout de tous les tags dans la div parent (bloc_photographers_tags)
+            bloc_photographers_tags.append(lien_photographers_tags);
+            lien_photographers_tags.append(photographers_tags);
+
+            //ajout de class à tous les éléments
+            lien_photographers_tags.setAttribute("class", "lien_tag_photographe");
+            photographers_tags.setAttribute("class", "photographers_tags");
+            lien_photographers_tags.setAttribute("href", "#");
+            photographers_tags.setAttribute("aria-label", photographer);
+        })
+    })
+}
+
+function getPhotographersFromTags(photographers, selectedTag) {
+    //boucle json et verifier si les tags correspondes 
+    return photographers.filter(photographer => {
+        return photographer.tags.filter((tag) => selectedTag.includes(tag)).length;
+    });
 }
 
--- src/photographe.js
+++ src/photographe.js
@@ -3,10 +3,165 @@ import data from '../data.json';
 //permet de recuperer les information passé dans l'url
 const url = new URL(window.location.href);
 const id = url.searchParams.get("id");
-console.log(id, data.photographers.find(photographer => photographer.id == id));
 
-// const doc = document.getElementById("bloc_main");
+console.log(data)
 
-// let img = document.createElement("img");
-// doc.append(img);
-// img.src = id.portrait;
+let galleries = data.media.filter(media => media.photographerId == id);
+
+let bloc = document.getElementById("bloc_main");
+
+let photographers = data.photographers.find(photographer => photographer.id == id);
+
+createImage(galleries)
+
+function createImage(galleries) {
+    bloc.innerHTML = "";
+    galleries.forEach((gallerie) => {
+        const bloc_img = document.createElement("div");
+        const bloc_info = document.createElement("div");
+        const img = document.createElement("img");
+        const title = document.createElement("p");
+        const price = document.createElement("p");
+        const like = document.createElement("p");
+    
+        bloc.append(bloc_img);
+        bloc_img.append(img);
+        bloc_img.append(bloc_info);
+        bloc_info.append(title);
+        bloc_info.append(price);
+        bloc_info.append(like);
+    
+        const imageDirectoryName = photographers.name.split(' ')[0];
+        if (gallerie.image && gallerie.image.includes(".jpg.jpg")) {
+            gallerie.image = gallerie.image.replace('.jpg.jpg', '.jpg')
+        }
+    
+        img.src = "../img/Sample_Photos-2/"+ imageDirectoryName+"/"+gallerie.image;
+
+        gallerie.name = gallerie.image ? gallerie.image.replace('.jpg', '') : gallerie.video.replace('.mp4', '');
+        title.innerHTML = gallerie.name;
+        price.innerHTML = gallerie.price+" €";
+        like.innerHTML = gallerie.likes + ' <i class="fas fa-heart"></i>';
+    
+        //class
+        bloc_img.setAttribute("class", "bloc-img mt-4");
+        bloc_info.setAttribute("class", "d-flex justify-content-between bloc_info mt-2");
+        title.setAttribute("class", "title")
+        img.setAttribute("class", "w-100 img-card");
+    
+        //aria-label
+        bloc_img.setAttribute("aria-label", gallerie.image)
+        title.setAttribute("aria-label", gallerie.image)
+        price.setAttribute("aria-label", gallerie.price+" euro");
+        img.setAttribute("alt", gallerie.image);
+        like.setAttribute("aria-label", gallerie.likes+" like");
+    });
+}
+
+const info = document.getElementById("info")
+const img = document.getElementById("img_photographer");
+
+//info
+const name = document.createElement("h2");
+const city = document.createElement("h3");
+const photographer_tagLine = document.createElement("p");
+const bloc_photographers_tags = document.createElement("div");
+
+info.append(name);
+info.append(city);
+info.append(photographer_tagLine);
+info.append(bloc_photographers_tags);
+
+name.innerHTML = photographers.name;
+city.innerHTML = photographers.city+ ", " + photographers.country;
+photographer_tagLine.innerHTML = photographers.tagline;
+
+//class
+name.setAttribute("class", "photographers");
+city.setAttribute("class", "photographers_city");
+bloc_photographers_tags.setAttribute("class", "bloc_photographers")
+
+//aria-label
+name.setAttribute("aria-label", photographers.name);
+city.setAttribute("aria-label", photographers.city);
+bloc_photographers_tags.setAttribute("aria-label", photographers.tagline)
+
+photographers.tags.forEach((photographer) => {
+    //création de tous les élements pour les tags de chaque photographe
+    const lien_photographers_tags = document.createElement("a");
+    const photographers_tags = document.createElement("p");
+
+    //affichage de tous les tags nécessaire pour chaque photographe
+    photographers_tags.textContent = "#"+ photographer;
+
+    //ajout de tous les tags dans la div parent (bloc_photographers_tags)
+    bloc_photographers_tags.append(lien_photographers_tags);
+    lien_photographers_tags.append(photographers_tags);
+
+    //ajout de class à tous les éléments
+    lien_photographers_tags.setAttribute("class", "lien_tag_photographe");
+    photographers_tags.setAttribute("class", "photographers_tags");
+    lien_photographers_tags.setAttribute("href", "#");
+    photographers_tags.setAttribute("aria-label", photographer);
+
+    //aria-label
+    photographers_tags.setAttribute("aria-label", photographer);
+}) 
+
+img.src = "../img/Sample_Photos-2/Photographers_ID_Photos/"+photographers.portrait;
+
+
+//dropdown
+
+let title_button = document.getElementById("dropdownMenuButton1");
+let titre = document.getElementById("titre");
+let popularité = document.getElementById("popularite");
+let date = document.getElementById("date");
+
+titre.addEventListener("click", () => {
+    title_button.innerHTML = titre.textContent;
+    galleries.sort((a, b) => a.name > b.name);
+    createImage(galleries)
+});
+popularité.addEventListener("click", () => {
+    title_button.innerHTML = popularité.textContent;
+    galleries.sort((a, b) => b.likes - a.likes);
+    createImage(galleries)
+});
+date.addEventListener("click", () => {
+    title_button.innerHTML = date.textContent;
+    galleries.forEach(gallerie => {
+        //transform en timestamp pour trier
+        gallerie.timestamp = (new Date(gallerie.date)).getTime()
+    });
+    galleries.sort((a, b) => b.timestamp - a.timestamp);
+    createImage(galleries)
+});
+
+// function test() {
+//     let carousel = `
+//     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+//         <div class="carousel-inner">
+//             <div class="carousel-item active">
+//                 <img src="../img/Sample_Photos-2/Event_PintoWedding.jpg" class="d-block w-100" alt="...">
+//             </div>
+//             <div class="carousel-item">
+//                 <img src="../img/Sample_Photos-2/Event_BenevidesWedding.jpg"" class="d-block w-100" alt="...">
+//             </div>
+//         </div>
+//         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+//             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+//             <span class="visually-hidden">Previous</span>
+//         </button>
+//         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+//             <span class="carousel-control-next-icon" aria-hidden="true"></span>
+//             <span class="visually-hidden">Next</span>
+//         </button>
+//     </div>
+// `;
+
+// document.getElementById("carousel").innerHTML += carousel;
+// }
+
+
+// test()
